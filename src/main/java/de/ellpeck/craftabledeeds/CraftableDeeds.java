597189b88f3d7c643f64ba81f91d0920ab29f538
Ellpeck <me@ellpeck.de>
2021-01-16 00:57:41 +0100

    setup

---
+++ src/main/java/de/ellpeck/craftabledeeds/CraftableDeeds.java
@@ -0,0 +1,13 @@
+package de.ellpeck.craftabledeeds;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CraftableDeeds.ID)
+public class CraftableDeeds {
+
+    public static final String ID = "craftabledeeds";
+
+    public CraftableDeeds() {
+
+    }
+}
